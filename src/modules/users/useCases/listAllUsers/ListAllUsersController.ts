1578a6f57a774df1fb7bf6d1a2b5dbebdb689777
Gedson Marcelino <gedson.marcelino@gmail.com>
2022-04-20 20:52:28 -0300

    feat: :sparkles: implemented list all users

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -6,7 +6,14 @@ class ListAllUsersController {
   constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const { user_id } = request.headers;
+    try {
+      const id = Array.isArray(user_id) ? user_id[0] : user_id;
+      const result = this.listAllUsersUseCase.execute({ user_id: id });
+      return response.send(result);
+    } catch (error) {
+      return response.status(400).json({ error });
+    }
   }
 }
 
